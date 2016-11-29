a805ce5b8e36aba04ffeeb80775e52677493a87b
Nesterov Ilya <nesterovilyan@gmail.com>
2016-11-29 20:32:30 +0300

    #7 Инициализация проекта, пример модульного теста.

---
+++ app-test/core/HelloWorldTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using app.core;
+
+namespace app_test.core
+{
+    [TestClass]
+    public class HelloWorldTest
+    {
+
+        [TestMethod]
+        public void Test_SayHello()
+        {
+            HelloWorld helloWorld = new HelloWorld();
+            Assert.AreEqual("Hello!", helloWorld.SayHello());
+        }
+
+
+    }
+}
