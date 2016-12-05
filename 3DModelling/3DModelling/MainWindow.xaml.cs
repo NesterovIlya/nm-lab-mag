575818a8ca46a2dc2291192e6688f033f8d9361e
MariiaDemchenko <masha-vrn@yandex.ru>
2016-12-06 01:26:27 +0200

    #13. Моделирование куба средствами Geometry3D

---
+++ 3DModelling/3DModelling/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+
+
+
+
+namespace _3DModelling
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MeshGeometry3D meshGeometry3D = new MeshGeometry3D();
+
+            meshGeometry3D.Positions.Add(new Point3D(0, 0, 1));
+            meshGeometry3D.Positions.Add(new Point3D(0, 1, 1));
+            meshGeometry3D.Positions.Add(new Point3D(0, 0, 0));
+            meshGeometry3D.Positions.Add(new Point3D(0, 1, 0));
+            meshGeometry3D.Positions.Add(new Point3D(1, 0, 1));
+            meshGeometry3D.Positions.Add(new Point3D(1, 1, 1));
+            meshGeometry3D.Positions.Add(new Point3D(1, 0, 0));
+            meshGeometry3D.Positions.Add(new Point3D(1, 1, 0));
+            
+
+            meshGeometry3D.TriangleIndices.Add(0);
+            meshGeometry3D.TriangleIndices.Add(1);
+            meshGeometry3D.TriangleIndices.Add(5);
+
+            meshGeometry3D.TriangleIndices.Add(0);
+            meshGeometry3D.TriangleIndices.Add(5);
+            meshGeometry3D.TriangleIndices.Add(4);
+
+            meshGeometry3D.TriangleIndices.Add(4);
+            meshGeometry3D.TriangleIndices.Add(5);
+            meshGeometry3D.TriangleIndices.Add(7);
+
+            meshGeometry3D.TriangleIndices.Add(4);
+            meshGeometry3D.TriangleIndices.Add(7);
+            meshGeometry3D.TriangleIndices.Add(6);
+
+            meshGeometry3D.TriangleIndices.Add(2);
+            meshGeometry3D.TriangleIndices.Add(3);
+            meshGeometry3D.TriangleIndices.Add(7);
+
+            meshGeometry3D.TriangleIndices.Add(2);
+            meshGeometry3D.TriangleIndices.Add(7);
+            meshGeometry3D.TriangleIndices.Add(6);
+
+            meshGeometry3D.TriangleIndices.Add(0);
+            meshGeometry3D.TriangleIndices.Add(1);
+            meshGeometry3D.TriangleIndices.Add(3);
+
+            meshGeometry3D.TriangleIndices.Add(0);
+            meshGeometry3D.TriangleIndices.Add(3);
+            meshGeometry3D.TriangleIndices.Add(2);
+
+            meshGeometry3D.TriangleIndices.Add(0);
+            meshGeometry3D.TriangleIndices.Add(2);
+            meshGeometry3D.TriangleIndices.Add(4);
+
+            meshGeometry3D.TriangleIndices.Add(4);
+            meshGeometry3D.TriangleIndices.Add(2);
+            meshGeometry3D.TriangleIndices.Add(6);
+
+            meshGeometry3D.TriangleIndices.Add(5);
+            meshGeometry3D.TriangleIndices.Add(1);
+            meshGeometry3D.TriangleIndices.Add(0);
+
+            meshGeometry3D.TriangleIndices.Add(7);
+            meshGeometry3D.TriangleIndices.Add(5);
+            meshGeometry3D.TriangleIndices.Add(0);
+
+
+            SolidColorBrush brush = new SolidColorBrush();
+            brush.Color = Colors.Red;
+            brush.Opacity = 1;
+            Material material = new DiffuseMaterial(brush);
+            
+
+            GeometryModel3D geometry = new GeometryModel3D(meshGeometry3D, material);
+                       
+            ModelUIElement3D model = new ModelUIElement3D();
+            model.Model = geometry;
+
+            myViewport.Children.Add(model);
+        }
+    }
+}
